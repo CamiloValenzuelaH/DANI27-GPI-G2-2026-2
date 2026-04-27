50b501363ed7f210ce9a0efcfe4c4915ddccfe35
Camilo Valenzuela <cnvh2003@gmail.com>
2026-04-27 16:33:23 -0400

    Frontend generico creado y conectado al backen end el area de login

---
+++ frontend/src/main.tsx
@@ -0,0 +1,8 @@
+
+  import { createRoot } from "react-dom/client";
+  //import App from "./app/AppPro.tsx";
+  import "./styles/index.css";
+import AppPro from "./app/AppPro.tsx";
+
+  createRoot(document.getElementById("root")!).render(<AppPro />);
+  
